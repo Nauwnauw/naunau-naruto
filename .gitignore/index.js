5a2b569d921ecfad5efc4cf67cfd0ed9c0373d80
Nauwnauw <49287058+Nauwnauw@users.noreply.github.com>
2019-04-05 23:58:05 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,26 +8,15 @@ bot.on('ready', function() {
     console.log("Connecté");
 });
 
-bot.login(process.env.TOKEN);
-
-bot.on("message", (message) => {
-    if (message.content.startsWith("*kick")) {
-        // Easy way to get member object though mentions.
-        var member= message.mentions.members.first();
-        // Kick
-        member.kick().then((member) => {
-            // Successmessage
-            message.channel.send(":wave: " + member.displayName + " a bien été kick :wink: :point_right: ");
-        }).catch(() => {
-             // Failmessage
-            message.channel.send("Tu ne peux pas faire ceci");
-        });
-    }
-});
+bot.login('process.env.TOKEN');
 
 bot.on('message', message => {
+    if (message.content === prefix + "Naunau"){
+        message.channel.sendMessage("Naunau il est **TROP** beau pas vrai ?")
+    }
+bot.on('message', message => {
     if (message.content === prefix + "help"){
-            message.channel.sendMessage("``` Voici la liste des commandes : \n  -*help \n  -Salut ( sans prefix ) \n  -*Naruto 1.0  -*fabriquer```");
+            message.channel.sendMessage("``` Voici la liste des commandes : \n  -*help \n  Salut ( sans prefix ) \n  -*Naruto 1.0  -*fabriquer \n *Naunau \n *abcd \n *ping ```");
     }
 
     if(message.content === "Salut"){
@@ -35,10 +24,27 @@ bot.on('message', message => {
         console.log("Commande Salut effectué");
     }
 
+    if(message.content === "*abcd") {
+        var embed = new Discord.RichEmbed()
+        .setDescription("Information du Discord")
+        .addField("Nom du discord", message.guild.name)
+        .addField("Crée le", message.guild.createdAt)
+        .addField("Utilisateurs sur le discord", message.guild.memberCount)
+        .setColor("0xFB7F04")
+    message.channel.sendEmbed(embed)
+    console.log("Commande Info Discord effectué.")
+
+    }
+
     if (message.content === prefix + "fabriquer"){
         message.reply("Ce bot a été crée par **Naunau**");
         console.log("commande fabriquant effectué");
     }
+
+    if (message.content === prefix + "ping"){
+        message.reply("Pong");
+        console.log("Commande ping effectué");
+    };
 
     if (message.content === prefix + "Naruto 1.0"){
         var embed = new Discord.RichEmbed()
@@ -48,5 +54,6 @@ bot.on('message', message => {
             .setColor("0xFB7F04")
             .setFooter("Cela vous a plus ? x)")
         message.channel.sendEmbed(embed);
+        }
     }
-});
+)});
