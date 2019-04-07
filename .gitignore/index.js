247691b837722c7584a97517065962b078acbf77
Nauwnauw <49287058+Nauwnauw@users.noreply.github.com>
2019-04-07 21:49:07 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,50 +10,41 @@ bot.on('ready', function() {
 
 bot.login('process.env.TOKEN');
 
+
 bot.on('message', message => {
     if (message.content === prefix + "Naunau"){
         message.channel.sendMessage("Naunau il est **TROP** beau pas vrai ?")
-    }
+    }})
+
+bot.on('message', message => {
+    if (message.content === prefix + "Compte De Naunau")
+    message.channel.sendMessage("```Voici ses identifiants : _Naunau_#6954```")
+})
+
+bot.on('message', message => {
+    if (message.content === prefix + "fabriquer")
+    message.channel.sendMessage("Ce bot a été fait par le valereux Naunau !")
+})
+
+bot.on('message', message => {
+    if (message.content === prefix + "Numéro De Téléphone")
+    message.channel.sendMessage("07 69 68 ..... Bonne Chance pour trouver les 2 derniers ! :wink: ")
+})
+
 bot.on('message', message => {
     if (message.content === prefix + "help"){
-            message.channel.sendMessage("``` Voici la liste des commandes : \n  -*help \n  Salut ( sans prefix ) \n  -*Naruto 1.0  \n *fabriquer \n *Naunau \n *abcd \n *ping ```");
-    }
-
-    if(message.content === "Salut"){
-        message.reply(" Hey !");
-        console.log("Commande Salut effectué");
-    }
-
-    if(message.content === "*abcd") {
         var embed = new Discord.RichEmbed()
-        .setDescription("Information du Discord")
-        .addField("Nom du discord", message.guild.name)
-        .addField("Crée le", message.guild.createdAt)
-        .addField("Utilisateurs sur le discord", message.guild.memberCount)
-        .setColor("0xFB7F04")
-    message.channel.sendEmbed(embed)
-    console.log("Commande Info Discord effectué.")
-
-    }
-
-    if (message.content === prefix + "fabriquer"){
-        message.reply("Ce bot a été crée par **Naunau**");
-        console.log("commande fabriquant effectué");
-    }
-
-    if (message.content === prefix + "ping"){
-        message.reply("Pong");
-        console.log("Commande ping effectué");
-    };
-
-    if (message.content === prefix + "Naruto 1.0"){
-        var embed = new Discord.RichEmbed()
-            .setTitle("ElNaruto")
-            .setDescription("On m'a dit que tu voulais voir une image de Naruto")
-            .addField("Regardez cette image magnifique ! (http://s2.noelshack.com/old/up/narutoclaire-fb7eba9440.jpg", true)
-            .setColor("0xFB7F04")
-            .setFooter("Cela vous a plus ? x)")
+            .setTitle("Voici les commandes de ce bot :")
+            .addField("Information :\n```fabriquer \nNaunau \nCompte De Naunau \nNuméro De Téléphone ```")
+            .setColor("0xFF8000")
+            .setFooter("Naunau")
         message.channel.sendEmbed(embed);
-        }
-    }
-)});
+    }})
+
+bot.on("guildMemberAdd", member => {
+    member.guild.channels.find("name", "général").send(`Bienvenue ${member}! J'espère que tu t'ammuseras sur ce serveur !`)
+})
+        
+bot.on("guildMemberRemove", member => {
+    member.guild.channels.find("name", "général").send("Aurevoir ${member}! J'espère que tu t'es ammusé sur le serveur !" )
+})
